cba4bc46bce3027185144969cdf9182ece7d678c
Ariel Silva <51482182+Alwexis@users.noreply.github.com>
2022-11-30 17:37:42 -0300

    Update script.sh

--- 1/script.sh
+++ 1/script.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+# Limpio el archivo
+> Respuesta.txt
+# Variables
 file='Ciudades.txt'
+base='https://wttr.in/'
+format='?format=3'
+# Bucle para leer el archivo
 while read line; do
-    curl --request GET "https://wttr.in/$line?format=3" >> Respuesta.txt
-done < $file  
-
-read -p "Presiona cualquier tecla para cerrar la terminal..." -n1 -s
+    # Obtengo la ciudad de la linea (y remplazo espacios por -, para evitar errores)
+    city=$(echo $line | tr ' ' '-')
+    # Obtengo el clima de la ciudad via cURL
+    curl $base$city$format >> Respuesta.txt
+done < $file
